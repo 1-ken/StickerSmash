b43ec09da50b5a37b79a4bac1a95c21d5a2b3986
1-ken <kennicmwimu@gmail.com>
2026-03-20 11:24:38 +0300

    button components and presable with font awesome

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,7 @@
-import { Text, View,StyleSheet } from "react-native";
-import Link from "expo-router/link";
-import { Image } from 'expo-image';
+import {  View,StyleSheet } from "react-native";
+
+import ImageViewer from "@/components/ImageViewer";
+import Button from "@/components/Button";
 
 const PlaceholderImage = require('@/assets/images/background-image.png');
 
@@ -8,7 +9,10 @@ export default function Index() {
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <Image source={PlaceholderImage} style={styles.image} />
+        <ImageViewer imgSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+  <Button theme="primary" label="Choose a photo" />        <Button label="Use this photo" />
       </View>
     </View>
   );
@@ -36,5 +40,9 @@ const styles = StyleSheet.create({
     width: 320,
     height: 440,
     borderRadius: 18,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
   },
 })
