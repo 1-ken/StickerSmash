1b0669064fc63d99f48ca837e11bd943b5a18133
1-ken <kennicmwimu@gmail.com>
2026-03-20 11:07:10 +0300

    assets added, viewd images added component to view the image

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,15 @@
 import { Text, View,StyleSheet } from "react-native";
 import Link from "expo-router/link";
+import { Image } from 'expo-image';
+
+const PlaceholderImage = require('@/assets/images/background-image.png');
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home screen</Text>
-      <Link href="/about" style={styles.button}>
-      go to the about screen
-      </Link>
+      <View style={styles.imageContainer}>
+        <Image source={PlaceholderImage} style={styles.image} />
+      </View>
     </View>
   );
 }
@@ -16,14 +18,23 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#25292e',
     alignItems: 'center',
-    justifyContent: 'center',
+    // justifyContent: 'center',
   },
-  text:{
-    color: '#fff',
+  // text:{
+  //   color: '#fff',
+  // },
+  // button:{
+  //   fontSize: 20,
+  //   textDecorationLine: 'underline',
+  //   color: '#fff',
+  // }
+
+  imageContainer: {
+    flex: 1,
   },
-  button:{
-    fontSize: 20,
-    textDecorationLine: 'underline',
-    color: '#fff',
-  }
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
 })
